9154ce7a2f0f366625820f9b07b1c87d01b7d413
frandedeseo <frandedeseo@gmail.com>
2025-05-07 19:59:28 -0300

    Trying with pydub

--- services/WpmService.py
+++ services/WpmService.py
@@ -1,10 +1,10 @@
-#from pydub import AudioSegment, silence
+from pydub import AudioSegment, silence
 import io
 
 class WpmService:
     def calculate(self, audio_bytes: bytes, text: str, min_silence_len: int = 2000, silence_thresh: int = -70) -> float:
-        #audio = AudioSegment.from_file(io.BytesIO(audio_bytes), format="wav")
-        #silent_ranges = silence.detect_silence(audio, min_silence_len=min_silence_len, silence_thresh=silence_thresh)
+        audio = AudioSegment.from_file(io.BytesIO(audio_bytes), format="wav")
+        silent_ranges = silence.detect_silence(audio, min_silence_len=min_silence_len, silence_thresh=silence_thresh)
         total_silence = 0
         dur_total_ms = 10000
         active_s = max(0, dur_total_ms - total_silence) / 1000
